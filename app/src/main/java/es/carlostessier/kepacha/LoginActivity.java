fd2836358044c87a563ca5fec025c43240374369
carlosfernandez <prof.carlos.tessier@gmail.com>
2014-12-29 21:46:53 +0100

    added menu logout and ProgressDialog

--- app/src/main/java/es/carlostessier/kepacha/LoginActivity.java
+++ app/src/main/java/es/carlostessier/kepacha/LoginActivity.java
@@ -1,19 +1,107 @@
 package es.carlostessier.kepacha;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 
 public class LoginActivity extends Activity {
+
+    private EditText usernameField;
+    private EditText passwordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeViews();
+    }
+
+
+    private void initializeViews() {
+        usernameField = (EditText) findViewById(R.id.usernameField);
+        passwordField = (EditText) findViewById(R.id.passwordField);
+    }
+
+
+    private void errorFieldDialog(String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setMessage(message);
+        builder.setPositiveButton(android.R.string.ok, null);
+        builder.setTitle(R.string.signup_error_title);
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+
+    }
+
+    public void actionLoginButton(View v){
+
+        String sUsername = usernameField.getText().toString().trim();
+        String sPassword = passwordField.getText().toString().trim();
+
+
+        String message ;
+
+        if (sUsername.isEmpty())
+        {
+            message = String.format(getString(R.string.empty_field_message),getString(R.string.username_hint));
+            errorFieldDialog(message);
+        }
+        else if (sPassword.isEmpty())
+        {
+            message = String.format(getString(R.string.empty_field_message),getString(R.string.password_hint));
+            errorFieldDialog(message);
+
+        }
+
+        else{
+            loginUser(sUsername, sPassword);
+        }
+
+
+
+    }
+
+
+    private void loginUser(final String username, String password) {
+
+
+        setProgressBarIndeterminateVisibility(true);
+
+
+      final ProgressDialog dialog = ProgressDialog.show(LoginActivity.this,
+                getString(R.string.loging_message),
+                getString(R.string.waiting_message), true);
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    dialog.dismiss();
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                } else {
+                    String message = String.format(getString(R.string.login_user_error_message), username);
+                    errorFieldDialog(message);
+                }
+            }
+        });
+
     }
 
     public void SignUpOnClick(View v){
--- app/src/main/java/es/carlostessier/kepacha/MainActivity.java
+++ app/src/main/java/es/carlostessier/kepacha/MainActivity.java
@@ -5,18 +5,25 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.parse.ParseUser;
+
 import java.util.Locale;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
+
+    final static String TAG = MainActivity.class.getName();
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -38,11 +45,19 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser == null) {
+            Log.d(TAG, "No está logueado");
+            navigateToLogin();
+        }
+        else{
+            Log.d(TAG,"usuario "+ currentUser.getUsername()+" logueado");
+        }
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
+
+        // Set up the action bar.
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -51,6 +66,28 @@ public class MainActivity extends ActionBarActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
     }
 
 
@@ -69,13 +106,41 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_logout) {
+            Log.d(TAG,"usuario "+ ParseUser.getCurrentUser().getUsername()+" desconectado");
+            ParseUser.logOut();
+            navigateToLogin();
             return true;
+        }
+        else{
+            Log.d(TAG,"unknown option menu");
+
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void navigateToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
